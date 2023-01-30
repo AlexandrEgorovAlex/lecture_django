4e62eb3909b845c127c2c1808a968c667d3dfe5f
Test <iehorov1995@gmail.com>
2023-01-30 16:16:51 +0200

    modifined views.py

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -5,9 +5,8 @@ from django.shortcuts import render
 tasks = ['foo', 'bar', 'baz']
 
 class NewTaskForm(forms.Form):
-	tasks = forms.CharField(label = "New Task")
-	priority = forms.IntegerField(label='Priority', min_value = 1, max_value = 5)
-
+	task = forms.CharField(label = "New Task")
+	
 # Create your views here.
 def index(request):
 	return render(request, 'tasks/index.html', {
@@ -18,7 +17,7 @@ def add(request):
 	if request.method == "POST":
 		form = NewTaskForm(request.POST)
 		if form.is_valid():
-			task = form.cleaned_data('task')
+			task = form.cleaned_data['task']
 			tasks.append(task)
 		else:
 			return render(request, 'tasks/add.html', {
